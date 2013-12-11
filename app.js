254d996aa5f799e89ae6f38606be3e83ab3a8303
sy6765856 <sy6765856@gmail.com>
2013-12-11 22:56:23 +0800

    12112256

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var http = require('http');
+var head = '<head><script type="text/javascript" src="./js/draw.js"></script></head>';
+console.log(head);
 var app = function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write('<title>Hello</title>');
+	res.write(head);
 	res.end('<body>Hello  dsd</body>');
 };
 http.createServer(app).listen(8000);
